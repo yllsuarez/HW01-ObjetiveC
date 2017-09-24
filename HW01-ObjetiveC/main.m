fdfeb3340328a056ef802f77813ad4fe1e91f654
yllsuarez <yonnyl@openvpn-client.sslvpn.interimhealthcare.com>
2017-09-24 18:14:38 -0400

    Initial Commit

---
+++ HW01-ObjetiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HW01-ObjetiveC
+//
+//  Created by Yonny L Leyva Suarez on 9/24/17.
+//  Copyright © 2017 Yonny L Leyva Suarez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
